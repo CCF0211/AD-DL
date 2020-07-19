974935864159c0fa2b2e4bd6a4074dad03af3428
CC0624 <991227156@qq.com>
2020-07-19 11:55:08 +0800

    fix print now/total epoch

--- clinicadl/clinicadl/tools/deep_learning/cnn_utils.py
+++ clinicadl/clinicadl/tools/deep_learning/cnn_utils.py
@@ -59,7 +59,7 @@ def train(model, train_loader, valid_loader, criterion, optimizer, resume, log_d
         if fi is not None and options.n_splits is not None:
             print("At (%d/%d) fold (%d/%d) epoch." % (fi, options.n_splits, epoch, options.epochs))
         else:
-            print("At fold (%d/%d) epoch." % (epoch, options.epochs))
+            print("At (%d/%d) epoch." % (epoch, options.epochs))
 
         model.zero_grad()
         evaluation_flag = True
