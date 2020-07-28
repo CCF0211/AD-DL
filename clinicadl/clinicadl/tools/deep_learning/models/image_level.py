47acf7eec750029a69f40b3fac90578ceb167408
CC0624 <991227156@qq.com>
2020-07-28 22:58:50 +0800

    add_DAM

--- clinicadl/clinicadl/tools/deep_learning/models/image_level.py
+++ clinicadl/clinicadl/tools/deep_learning/models/image_level.py
@@ -2,11 +2,75 @@
 
 from .modules import PadMaxPool3d, Flatten
 import torch.nn as nn
+import sys
+
+sys.path.append('/root/fanchenchen/fanchenchen/MRI/code/paper')
+from networks import DAM_3d
 
 """
 All the architectures are built here
 """
 
+class Conv5_FC3_DAM(nn.Module):
+    """
+    Classifier for a binary classification task
+
+    Image level architecture used on Minimal preprocessing
+    """
+
+    def __init__(self, dropout=0.5):
+        super(Conv5_FC3, self).__init__()
+        self.DAM = DAM_3d(1, 1)
+        self.features = nn.Sequential(
+            nn.Conv3d(1, 8, 3, padding=1),
+            nn.BatchNorm3d(8),
+            nn.ReLU(),
+            PadMaxPool3d(2, 2),
+
+            nn.Conv3d(8, 16, 3, padding=1),
+            nn.BatchNorm3d(16),
+            nn.ReLU(),
+            PadMaxPool3d(2, 2),
+
+            nn.Conv3d(16, 32, 3, padding=1),
+            nn.BatchNorm3d(32),
+            nn.ReLU(),
+            PadMaxPool3d(2, 2),
+
+            nn.Conv3d(32, 64, 3, padding=1),
+            nn.BatchNorm3d(64),
+            nn.ReLU(),
+            PadMaxPool3d(2, 2),
+
+            nn.Conv3d(64, 128, 3, padding=1),
+            nn.BatchNorm3d(128),
+            nn.ReLU(),
+            PadMaxPool3d(2, 2),
+
+        )
+
+        self.classifier = nn.Sequential(
+            Flatten(),
+            nn.Dropout(p=dropout),
+
+            nn.Linear(128 * 6 * 7 * 6, 1300),
+            nn.ReLU(),
+
+            nn.Linear(1300, 50),
+            nn.ReLU(),
+
+            nn.Linear(50, 2)
+
+        )
+
+        self.flattened_shape = [-1, 128, 6, 7, 6]
+
+    def forward(self, x):
+        x = self.DAM(x)
+        x = self.features(x)
+        x = self.classifier(x)
+
+        return x
 
 class Conv5_FC3(nn.Module):
     """
@@ -14,9 +78,9 @@ class Conv5_FC3(nn.Module):
 
     Image level architecture used on Minimal preprocessing
     """
+
     def __init__(self, dropout=0.5):
         super(Conv5_FC3, self).__init__()
-
         self.features = nn.Sequential(
             nn.Conv3d(1, 8, 3, padding=1),
             nn.BatchNorm3d(8),
@@ -74,6 +138,7 @@ class Conv5_FC3_mni(nn.Module):
 
     Image level architecture used on Extensive preprocessing
     """
+
     def __init__(self, dropout=0.5):
         super(Conv5_FC3_mni, self).__init__()
 
