965a5fedc29174685b2f572f29c95206708cdbb9
CC0624 <991227156@qq.com>
2020-07-29 22:29:35 +0800

    add DAM_last

--- clinicadl/clinicadl/tools/deep_learning/models/image_level.py
+++ clinicadl/clinicadl/tools/deep_learning/models/image_level.py
@@ -136,6 +136,70 @@ class Conv5_FC3_DAM_all_layer(nn.Module):
 
         return x
 
+class Conv5_FC3_DAM_last(nn.Module):
+    """
+    Classifier for a binary classification task
+
+    Image level architecture used on Minimal preprocessing
+    """
+
+    def __init__(self, dropout=0.5):
+        super(Conv5_FC3_DAM_last, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv3d(1, 8, 3, padding=1),
+            nn.BatchNorm3d(8),
+            nn.ReLU(),
+            # DAM_3d(8, 8, Sagittal_plane=169, Coronal_plane=208, Axial_plane=179),
+            PadMaxPool3d(2, 2),
+
+            nn.Conv3d(8, 16, 3, padding=1),
+            nn.BatchNorm3d(16),
+            nn.ReLU(),
+            # DAM_3d(16, 16, Sagittal_plane=85, Coronal_plane=104, Axial_plane=90),
+            PadMaxPool3d(2, 2),
+
+            nn.Conv3d(16, 32, 3, padding=1),
+            nn.BatchNorm3d(32),
+            nn.ReLU(),
+            # DAM_3d(32, 32, Sagittal_plane=43, Coronal_plane=52, Axial_plane=45),
+            PadMaxPool3d(2, 2),
+
+            nn.Conv3d(32, 64, 3, padding=1),
+            nn.BatchNorm3d(64),
+            nn.ReLU(),
+            # DAM_3d(64, 64, Sagittal_plane=22, Coronal_plane=26, Axial_plane=23),
+            PadMaxPool3d(2, 2),
+
+            nn.Conv3d(64, 128, 3, padding=1),
+            nn.BatchNorm3d(128),
+            nn.ReLU(),
+            DAM_3d(128, 128, Sagittal_plane=11, Coronal_plane=13, Axial_plane=12),
+            PadMaxPool3d(2, 2),
+
+        )
+
+        self.classifier = nn.Sequential(
+            Flatten(),
+            nn.Dropout(p=dropout),
+
+            nn.Linear(128 * 6 * 7 * 6, 1300),
+            nn.ReLU(),
+
+            nn.Linear(1300, 50),
+            nn.ReLU(),
+
+            nn.Linear(50, 2)
+
+        )
+
+        self.flattened_shape = [-1, 128, 6, 7, 6]
+
+    def forward(self, x):
+        x = self.features(x)
+        x = self.classifier(x)
+
+        return x
+
 
 class Conv5_FC3(nn.Module):
     """
