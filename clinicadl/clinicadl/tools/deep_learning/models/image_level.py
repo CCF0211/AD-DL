184d33d7716a4cf349835c9870efc1666cd4066d
CC0624 <991227156@qq.com>
2020-09-05 23:57:03 +0800

    add Nnet

--- clinicadl/clinicadl/tools/deep_learning/models/image_level.py
+++ clinicadl/clinicadl/tools/deep_learning/models/image_level.py
@@ -6,6 +6,7 @@ import sys
 
 sys.path.append('/root/fanchenchen/fanchenchen/MRI/code/paper')
 from networks import DAM_3d, UNet3D
+from Unet import N_Net
 from resnet50_3d import i3_res50, i3_res50_nl
 
 """
